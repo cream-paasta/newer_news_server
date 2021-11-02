235baf160d56bda56a6f07c11607dcb8c4307820
길상혁 <kill5038@gmail.com>
2021-11-03 01:19:46 +0900

    devise gem 설치 후 user모델 관련 기본 설정 추가

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  scope :api do
+    devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', registration: 'signup' },
+               controllers: { sessions: 'api/users/sessions', registrations: 'api/users/registrations' }
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
